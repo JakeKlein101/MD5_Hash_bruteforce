430eb5054f19b516886e5262fe06fa162bfb4d7b
Jake Klein <jakeklein91@gmail.com>
2020-11-09 15:27:10 +0200

    Working progress

--- Multi - thread/client.py
+++ Multi - thread/client.py
@@ -3,12 +3,15 @@ from hashlib import md5
 import os
 import pickle
 from consts import *
+from threading import Thread
 
 
 class Client:
     def __init__(self):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._cpu_cores = os.cpu_count()
+        self._list_ranges_per_thread = []
+        self._given_ranges_global = []
 
     def start(self):
         """
@@ -19,17 +22,37 @@ class Client:
         print(f"connected to server. IP:{SERVER_IP_ADDRESS}, PORT:{SERVER_PORT}.")
         self.main_loop()
 
-    def try_decode(self):
+    @staticmethod
+    def try_decode(range_tuple):
         """
-        1. Receives the edges of the range it needs to process from server.
+        1. Receives the edges of the range it needs to process from the ranges list.
         2. Goes through the given range and converts each number to MD5 and compares to given hash to check answer.
         3. returns the correct number if it finds it, otherwise 0.
+        4. The program runs multiple instances of this function in parallel to maximise results.
         """
-        range_edges_tuple = pickle.loads(self._sock.recv(BUFFER_SIZE))
-        for num in range(range_edges_tuple[0], range_edges_tuple[1]):
-            print(num)
+        for num in range(range_tuple[0], range_tuple[1]):
+            # print(num)
             if md5(str(num).encode()).hexdigest().upper() == CODE:
                 return num
+        return 0
+
+    def allocate_sub_range(self):
+        """
+        Splits the given range into even smaller ranges and places them into a list of tuples.
+        """
+        self._list_ranges_per_thread = []
+        for num in range(0, self._cpu_cores):
+            start = self._given_ranges_global[0]
+            finish = start + ((self._cpu_cores**10) // 2)
+            self._list_ranges_per_thread.append(tuple([start, finish]))
+            self._given_ranges_global[0] = finish
+            print(self._list_ranges_per_thread)
+
+    def thread_setup(self):  # TODO: how to get returns from threads????
+        for rng in self._list_ranges_per_thread:
+            calc_thread = Thread(target=self.try_decode, args=rng)
+            calc_thread.start()
+            calc_thread.join()
         return 0
 
     def main_loop(self):
@@ -38,7 +61,9 @@ class Client:
         2. Calls the try_decode function and returns the result to the server.
         """
         self._sock.sendall(pickle.dumps(tuple([self._cpu_cores])))  # initial message
-        result = self.try_decode()
+        self._given_ranges_global = list(pickle.loads(self._sock.recv(BUFFER_SIZE)))
+        self.allocate_sub_range()
+        result = self.thread_setup()  # TODO: put the results of all the threads into a list take the max num.
         if result != 0:
             self._sock.sendall(pickle.dumps(tuple([result])))
         else:
--- Multi - thread/server.py
+++ Multi - thread/server.py
@@ -2,3 +2,99 @@ import socket
 from threading import Thread
 import pickle
 from consts import *
+
+
+class Client(Thread):
+    def __init__(self, client_socket, server):
+        Thread.__init__(self)
+        self._client_socket = client_socket
+        self._cpu_cores = 0
+        self._range_start_finish = []
+        self._server = server
+
+    def run(self):
+        """
+        Overrides the run method in the thread parent class and initiates the thread.
+        Using the start method on a object inheriting from Thread calls the run method.
+        """
+        self.main_loop()
+
+    def main_loop(self):
+        """
+        1. First, receives initial data with the given function.
+        2. Allocates the range using the given function.
+        3. sends a tuple of the range edges to the thread.
+        4. Receives answer from thread, if the tuple is 0 then the thread failed, otherwise it succeeded.
+        """
+        self.receive_initial_data()
+        self.allocate_range()
+        print(f"ranges are: start -> {self._range_start_finish[0]}, finish -> {self._range_start_finish[1]}")  # debug
+        self._client_socket.sendall(pickle.dumps(tuple(self._range_start_finish)))
+        print("ranges sent")
+        try:
+            encoded_packet = self._client_socket.recv(BUFFER_SIZE)
+        except ConnectionResetError as e:
+            print("Thread disconnected")
+        else:
+            packet = pickle.loads(encoded_packet)
+            print("answer received")
+            # if the tuple has only a 0 its a failed attempt, if it has a number its the answer.
+            if packet[0] != 0:
+                print(f"The result is:{packet[0]}")
+            else:
+                print("No matches in thread")
+
+    def receive_initial_data(self):
+        """
+        receives the initial package from thread and updates the cpu count in the thread.
+        """
+        received_initial_packet = pickle.loads(self._client_socket.recv(BUFFER_SIZE))
+        # Initial packet contains only core amount
+        self._cpu_cores = received_initial_packet[0]
+
+    def allocate_range(self):
+        """
+        allocates the range to each thread by the amount of cores in the thread
+        and updates the main range to not have the part that was sent.
+        """
+        start = self._server._edges[0]
+        finish = int(self._server._edges[0] + (self._cpu_cores ** 10) / 2)
+        self._range_start_finish = [start, finish]
+        self._server._edges[0] = finish
+
+
+class Server:
+    def __init__(self):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._client_list = []
+        self._edges = [10 ** 9, (10 ** 10) - 1]
+
+    def start(self):
+        """
+        Binds the server to an IP and PORT and starts listening to connections, initiates main loop.
+        """
+        self._sock.bind((SERVER_IP_ADDRESS, SERVER_PORT))
+        self._sock.listen(1)
+        print(f"Server has been binded to the IP: {SERVER_IP_ADDRESS} port: {SERVER_PORT}")
+        self.server_main_loop()
+
+    def server_main_loop(self):
+        """
+        accepts a client connection and creates a new client object and starts it.
+        """
+        while True:
+            client_socket, client_address = self._sock.accept()
+            print(f"{client_address} just connected")
+            client = Client(client_socket, self)
+            self._client_list.append(client)
+            client.start()
+
+
+def main():
+    server = Server()
+    server.start()
+
+
+if __name__ == '__main__':
+    main()
+
